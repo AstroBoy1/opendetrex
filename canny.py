c0acad159468e42b172b2c8eb88072e8c573c1bf
omorim <omorim@oregonstate.edu>
2023-05-14 15:57:05 -0700

    canny np bool array

--- canny.py
+++ canny.py
@@ -3,6 +3,26 @@ import cv2 as cv
 from matplotlib import pyplot as plt
 
 
+def check_canny():
+    """Check canny edges were saved properly"""
+    fn = "../PROB/data/VOC2007/ImageSets/Main/train.txt"
+    dir = "../PROB/data/VOC2007/JPEGImages/"
+    out_dir = "../PROB/data/VOC2007/CannyImages/"
+    with open(fn) as fp:
+        im_ids = fp.readlines()
+    im_ids = [x.rstrip() for x in im_ids]
+    for i, f in enumerate(im_ids):
+        edge_file = out_dir + f + ".npy"
+        file = dir + f + ".jpg"
+        img = cv.imread(file, cv.IMREAD_GRAYSCALE)
+        with open(edge_file, "rb") as fp:
+            edges_array = np.load(fp)
+        edges = cv.Canny(img, 100, 200) / 255
+
+        if not np.array_equal(edges, edges_array.astype("bool")):
+            print(i, f)
+
+
 def main():
     """Run canny edges on training images and use for the new loss function"""
     fn = "../PROB/data/VOC2007/ImageSets/Main/train.txt"
@@ -20,9 +40,13 @@ def main():
             # plt.subplot(131),plt.imshow(img,cmap = 'gray')
             # plt.title('Original Image'), plt.xticks([]), plt.yticks([])
             edges = cv.Canny(img, 100, 200)
+            edges_array = edges / 255
             # plt.subplot(132),plt.imshow(edges,cmap = 'gray')
             # plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-            cv.imwrite(out_dir+f+".jpg", edges)
+            # jpg has compression, so does png...
+            #cv.imwrite(out_dir+f+".png", edges)
+            with open(out_dir+f+".npy", "wb") as fp:
+                np.save(fp, edges_array.astype("bool"))
         #breakpoint()
         except:
             print("error")
---
+++ projects/dino/configs/dino_r50_4scale_edge_loss.py
@@ -0,0 +1,78 @@
+from detrex.config import get_config
+from .models.dino_r50 import model
+
+# get default config
+dataloader = get_config("common/data/ood.py").dataloader
+#dataloader = get_config("common/data/coco_detr.py").dataloader
+optimizer = get_config("common/optim.py").AdamW
+lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_12ep
+train = get_config("common/train.py").train
+
+# modify training config
+#train.init_checkpoint = "detectron2://ImageNetPretrained/torchvision/R-50.pkl"
+#train.init_checkpoint = "./output/dino_agnostic_edges/model_0084999.pth"
+train.output_dir = "./output/edge_loss"
+
+# max training iterations, batch size of 4, 16,551 examples
+# 16551 / 4 = 4137.75 iterations per epoch
+# 90000 / 4137.75 = 21 epochs
+#train.max_iter = 90000
+train.max_iter = 180000
+
+# fast debug train.max_iter=20, train.eval_period=10, train.log_period=1
+train.fast_dev_run.enabled = False
+# Mixed precision training saves so much memory!
+train.amp = dict(enabled=True)
+
+# run evaluation every 5000 iters
+train.eval_period = 5000
+
+# log training infomation every 20 iters
+train.log_period = 200
+
+# save checkpoint every 5000 iters
+train.checkpointer.period = 5000
+
+# gradient clipping for training
+train.clip_grad.enabled = True
+train.clip_grad.params.max_norm = 0.1
+train.clip_grad.params.norm_type = 2
+
+# set training devices
+train.device = "cuda"
+model.device = train.device
+
+# known and unknown class
+model.num_classes = 2
+#model.select_box_nums_for_evaluation = 300
+
+# Frequency channel
+model.backbone.stem.in_channels = 4
+
+# dropout parameters
+model.transformer.encoder.attn_dropout = 0.1
+model.transformer.encoder.ffn_dropout = 0.1
+model.transformer.decoder.attn_dropout = 0.1
+model.transformer.decoder.ffn_dropout = 0.1
+model.criterion.matcher.cost_class = 1.0
+
+model.dn_number = 1
+#model.num_queries = 100
+
+# modify optimizer config
+optimizer.lr = 1e-4
+optimizer.betas = (0.9, 0.999)
+optimizer.weight_decay = 1e-4
+optimizer.params.lr_factor_func = lambda module_name: 0.1 if "backbone" in module_name else 1
+
+# modify dataloader config
+dataloader.train.num_workers = 16
+
+# Powers of 2
+# please notice that this is total batch size.
+# surpose you're using 4 gpus for training and the batch size for
+# each gpu is 16/4 = 4
+dataloader.train.total_batch_size = 8
+
+# dump the testing results into output_dir for visualization in tensorboard
+dataloader.evaluator.output_dir = train.output_dir
