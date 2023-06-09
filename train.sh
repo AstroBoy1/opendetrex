1de0e72949a8b7be129f5f260000f65f603ffe4a
omorim <omorim@oregonstate.edu>
2023-06-09 13:52:14 -0700

    t1 finetune

--- train.sh
+++ train.sh
@@ -1,2 +1,3 @@
 python tools/train_net.py --config-file projects/dino/configs/dino_r50_4scale_scratch.py \
-                          --num-gpus 2
+                          --num-gpus 2 \
+                          --dist-url tcp://127.0.0.1:12345 \
