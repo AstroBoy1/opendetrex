e5e689ae46c4d104e9bea148cda14ba5c8484ac4
omorim <omorim@oregonstate.edu>
2023-08-18 09:52:07 -0700

    d3 t3 nothing

--- open_eval/open_eval.py
+++ open_eval/open_eval.py
@@ -114,8 +114,8 @@ class PascalVOCDetectionEvaluator(DatasetEvaluator):
         PSEUDO_LABEL_KNOWN = False
         pseudo_box_fn = "pseudolabels/d3/t3/known/boxes_"
         pseudo_score_fn = "pseudolabels/d3/t3/known/scores_"
-        PREVIOUS_KNOWN = 15
-        NUM_CLASSES = PREVIOUS_KNOWN +5
+        PREVIOUS_KNOWN = 19
+        NUM_CLASSES = PREVIOUS_KNOWN + 1
 
         # For dataset 3
         save_exemplars = False
--- pascal_voc_coco.py
+++ pascal_voc_coco.py
@@ -79,10 +79,10 @@ def load_voc_instances(dirname: str, split: str, class_names: Union[List[str], T
     """
 
     UNKNOWN = False
-    PREV_KNOWN = 15
+    PREV_KNOWN = 19
     EXEMPLAR = False
     PSEUDO = False
-    NUM_CLASSES = PREV_KNOWN + 5
+    NUM_CLASSES = PREV_KNOWN + 1
 
     with PathManager.open(os.path.join(dirname, "ImageSets", "Main", split + ".txt")) as f:
         fileids = np.loadtxt(f, dtype=np.str)
